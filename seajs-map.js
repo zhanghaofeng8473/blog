37e812362c31dc89b3e324164c3f7cb295875619
lifesinger <lifesinger@gmail.com>
2011-11-02 15:25:27 +0800

    normal commit

--- seajs-map.js
+++ seajs-map.js
@@ -6,36 +6,38 @@ seajs.config({
 
 define(function() {
 
-  var rules = [];
+  var rules = [], xrules = [];
 
 
-  // markzhi
+  // for markzhi
   rules.push([
     'http://markzhi.com/assets/',
     'http://localhost/~lifesinger/markzhi/portal/src/main/webapp/assets/'
   ]);
 
 
-  // tms
+  // for tms
   rules.push([
     'http://assets.taobaocdn.com/apps/tms/',
     'http://localhost/~lifesinger/svn/assets/apps/tms/'
   ]);
 
-  // bigrender
+
+  // for bigrender
   rules.push([
     'http://a.tbcdn.cn/p/shop/1.0/bigrender.js',
     'http://localhost/~lifesinger/svn/assets/p/shop/1.0/bigrender.js'
   ]);
+
   
-  // modules
-//  rules.push([
-//    /^http:\/\/a\.tbcdn\.cn\/libs\/(.*)\.js$/,
-//    'http://localhost/~lifesinger/modules/$1-debug.js'
-//  ]);
+  // for libs
+  xrules.push([
+    /^http:\/\/a\.tbcdn\.cn\/libs\/(.*)\.js$/,
+    'http://localhost/~lifesinger/modules/$1-debug.js'
+  ]);
 
 
-  // map rules
+  // set map rules
   seajs.config({'map': rules});
 
 });
