7c6b30b890e49100c77b88223efef1c9cb9fe84e
lifesinger <lifesinger@gmail.com>
2012-10-25 11:49:02 +0800

    Update user scripts

--- config/ali.user.js
+++ config/ali.user.js
@@ -1,27 +1,34 @@
 // ==UserScript==
-// @name             Ali User Script
+// @name             User Script for Alipay
 // @namespace        lifesinger
 // @description      Do boring things automatically, and free your fingers.
 // @author           lifesinger@gmail.com
 // @source           http://lifesinger.github.com/config/ali.user.js
-// @install          http://lifesinger.github.com/config/ali.user.js
 // @match            http://*.alipay-inc.com/*
-// @version          1.0.0
+// @version          1.0.1
 // ==/UserScript==
 
 (function() {
 
-  var $ = document.querySelector
   var pageUrl = location.href
 
   // Remove the restriction for meeting system.
   if (pageUrl.indexOf('MeetingRoomBookingEdit.aspx') > 0) {
-    var comments = $('#ctl00_ContentPlaceHolder1_tb_comments')
-    comments.value = '团队开会'
-
     var submit = $('#ctl00_ContentPlaceHolder1_btn_booking')
     submit.onclick = null
     submit.value += '无忧'
+
+    var dialog = $('#ctl00_ContentPlaceHolder1_dialog')
+    if (dialog.style.display === 'block') {
+      location.href = '/MyBooking.aspx'
+    }
+  }
+
+
+  // Helpers
+
+  function $(selector) {
+    return document.querySelector(selector)
   }
 
 })()
