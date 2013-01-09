8590c1d32da0ff5b6f8927283a33d6eb2113dce2
lifesinger <lifesinger@gmail.com>
2013-01-10 00:14:06 +0800

    Update active.md

--- lab/2013/github-users/converter.js
+++ lab/2013/github-users/converter.js
@@ -15,8 +15,8 @@ stats.forEach(function(stat, index) {
   out += '<tr>'
   out += '<th scope="row">##' + (index + 1) + ': '
   out += '<a href="https://github.com/' + stat.username + '">' + stat.username + '</a></th>'
-  out += '</td>' + stat.contributions + '</td>'
-  out += '</td>' + stat.location + '</td>'
+  out += '<td>' + stat.contributions + '</td>'
+  out += '<td>' + stat.location + '</td>'
   out += '</tr>\n'
 })
 
