432cdac085ff900c3defdf7cd65f86dcdc230274
lifesinger <lifesinger@gmail.com>
2013-07-24 12:16:28 +0800

    Update chrome script

--- config/dev.user.js
+++ config/dev.user.js
@@ -3,15 +3,14 @@
 // @namespace        lifesinger
 // @description      Do boring things automatically, and free your fingers.
 // @author           lifesinger@gmail.com
-// @source           http://lifesinger.github.com/config/dev.user.js
-// @match            file://localhost/Users/lifesinger/Sites/*
-// @version          1.0.2
+// @match            file://*/*
+// @version          1.0.13
 // ==/UserScript==
 
-(function() {
-  var href = location.href
-
-  if (href.indexOf('?file') === -1) {
-    location.href = href.replace('file://localhost/Users/lifesinger/Sites/', 'http://localhost/~lifesinger/')
+(function(href) {
+  if (href.indexOf('file:///Users/lifesinger/Sites/') === 0) {
+    location.href = href.replace(
+        'file:///Users/lifesinger/Sites/',
+        'http://localhost/~lifesinger/')
   }
-})()
+})(location.href)
